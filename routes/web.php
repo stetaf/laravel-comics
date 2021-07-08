0189349b16f7906156464fd85bea96f1f4c4ff7f
stetaf <s.tafuri95@gmail.com>
2021-07-08 16:50:59 +0200

    Return home view with comics data

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $comics = config('comics');
+
+    $data = [
+        'comics' => $comics
+    ];
+
+    return view('home', $data);
 });
