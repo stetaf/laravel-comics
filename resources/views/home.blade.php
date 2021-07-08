a0c1ac164f718220c359f1d21aa88c94b3d52554
stetaf <s.tafuri95@gmail.com>
2021-07-08 17:49:34 +0200

    Add icons div

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,15 @@
 @extends('layout.app')
 
 @section('main')
+<!-- Jumbotron -->
 <div class="jumbotron">
     <div class="container">
         <span>Current series</span>
     </div>
 </div>
+<!-- /Jumbotron -->
+
+<!-- Comics -->
 <div class="comics">
     <div class="container">
         <div class="cards">
@@ -19,4 +23,32 @@
         </div>
     </div>
 </div>
+<!-- /Comics -->
+
+<!-- Icons -->
+<div class="icons">
+    <div class="container">
+        <div>
+            <img src="../img/buy-comics-digital-comics.png" alt="Digital comics" width="40" height="55">
+            <span>Digital comics</span>
+        </div>
+        <div>
+            <img src="../img/buy-comics-merchandise.png" alt="Digital comics" width="50" height="55">
+            <span>DC merchandise</span>
+        </div>
+        <div>
+            <img src="../img/buy-comics-subscriptions.png" alt="Digital comics" width="50" height="55">
+            <span>Subscription</span>
+        </div>
+        <div>
+            <img src="../img/buy-comics-shop-locator.png" alt="Digital comics" width="35" height="55">
+            <span>Comic shop locator</span>
+        </div>
+        <div>
+            <img src="../img/buy-dc-power-visa.svg" alt="Digital comics" width="50" height="45">
+            <span>Dc power visa</span>
+        </div>
+    </div>
+</div>
+<!-- /Icons -->
 @endsection
