30b7f742cbbc06452352d2a4f0a210f919a212ec
stetaf <s.tafuri95@gmail.com>
2021-07-08 15:26:29 +0200

    Add webpack directives

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+ mix.js('resources/js/app.js', 'public/js')
+ .copyDirectory('resources/img', 'public/img')
+ .options({
+     processCssUrls: false
+ })
+ .sass('resources/sass/app.scss', 'public/css');
