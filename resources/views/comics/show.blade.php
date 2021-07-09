22903a87e148e83798acb05f261438d4403245f2
stetaf <s.tafuri95@gmail.com>
2021-07-09 12:59:13 +0200

    Fix comic card

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -7,13 +7,18 @@
 <!-- /Jumbotron -->
 
 <div class="comic">
-    <div class="container">
-        <div class="comic_card">
+    <div class="comic_card">
+        <div class="container">
             <div class="img">
                 <img src="{{ $comic['thumb'] }}" alt="">
                 <span class="type">{{ $comic['type'] }}</span>
                 <span class="gallery">View gallery</span>
             </div>
+        </div>
+    </div>
+    <div class="comic_info">
+        <div class="container">
+            <h2>{{ $comic['title'] }}</h2>
         </div>
     </div>
 </div>
